216a6c346e1fe81c863579ac8192a4f07bffef2d
MarkCX <49326294+MarkCX1@users.noreply.github.com>
2025-06-20 20:16:58 -0400

    Delete

--- DMALib/ESP.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Header.h"
-
-bool WorldToScreen(Vec3 pos, Vec2& screen, float matrix[16], int windowWidth, int windowHeight);
-void DrawESP(DMAHandler* dma, HWND hwnd, DWORD baseAddress, DWORD entityList);
--- DMALib/Player.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Header.h"
-
-void RunHealthMod(DMAHandler* dma, DWORD_PTR localPlayer);
-void RunAmmoMod(DMAHandler* dma, DWORD_PTR localPlayer);
